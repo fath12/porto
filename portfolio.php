ab32ad6cc8a9b6d07b9e6d9bb11379834aced481
fath12 <mfatkh12@gmail.com>
2023-07-29 18:50:51 +0700

    portofolio menu
    code for the portofolio menu

---
+++ portfolio.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Portfolio - Your Name</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <h1>My Portfolio</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="profile.php">Profile</a></li>
+                <li><a href="question2.php">Question 2</a></li>
+                <li><a href="question3.php">Question 3</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section>
+        <!-- Add your portfolio projects here -->
+        <h2>Portfolio Projects</h2>
+        <ul>
+            <li>
+                <h3>Sistem Rekomendasi Smartphone Gaming</h3>
+                <p>Sebuah aplikasi berbasis website yang dibangun menggunakan python dan flask dengan
+                    menggabungkan metode Simple Additive Weighting dan Content Based Filtering.
+                </p>
+            </li>
+        </ul>
+    </section>
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> My Portfolio. All rights reserved.</p>
+    </footer>
+</body>
+</html>
